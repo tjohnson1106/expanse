48971a16a3df64fa5c2ba67354c98f3cf5aa3f53
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-22 17:17:05 -0400

    created rounded button component

--- src/components/ui-elements/buttons/RoundedButton.js
+++ src/components/ui-elements/buttons/RoundedButton.js
@@ -1,10 +1,41 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
 
-export const RoundedButton = () => {
-  return (
-    <View>
-      <Text>Rounded Button</Text>
-    </View>
-  );
+import colors from "../../../util/styles/colors";
+
+import PropTypes from "prop-types";
+
+class RoundedButton extends Component {
+  state = {};
+  render() {
+    const { text, color, background } = this.props;
+
+    return (
+      <TouchableHighlight style={styles.root}>
+        <Text style={[{ color }, styles.buttonText]}>{text}</Text>
+      </TouchableHighlight>
+    );
+  }
+}
+
+RoundedButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string
 };
+
+const styles = StyleSheet.create({
+  root: {
+    display: "flex",
+    padding: 15,
+    borderRadius: 40,
+    borderWidth: 1,
+    borderColor: colors.white
+  },
+  buttonText: {
+    fontSize: 16,
+    width: "100%",
+    textAlign: "center"
+  }
+});
+
+export default RoundedButton;
