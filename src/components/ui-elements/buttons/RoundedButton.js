c07c76baf3f3c9ae2276c81fcdb8d3d40299a23a
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2019-03-23 23:03:34 -0400

    merge

--- src/components/ui-elements/buttons/RoundedButton.js
+++ src/components/ui-elements/buttons/RoundedButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
+import PropTypes from "prop-types";
 
 export const RoundedButton = () => {
   return (
